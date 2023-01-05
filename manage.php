e35afa122f02b2b77d0c2af7e5467da33347f897
Darshan-webapp <darshanpurohit1803@gmail.com>
2023-01-05 20:38:34 +0530

    reindented code and separated navbar

--- manage.php
+++ manage.php
@@ -1,9 +1,9 @@
 <?php 
-    require_once('server/db.php');
+require_once('server/db.php');
 
-    $sql = "SELECT * FROM products";
-    $query = mysqli_query($conn, $sql);
-        
+$sql = "SELECT * FROM products";
+$query = mysqli_query($conn, $sql);
+
 ?>
 <html>
 <head>
@@ -14,61 +14,40 @@
 </head>
 <body>
     <!--navbar section-->
-    
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">Crud-app</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link" aria-current="page" href="index.php">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="manage.php">Show-Data</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Pricing</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
-        <div class="container mt-2">
+    <?php require_once('navbar.php'); ?>
+    <div class="container mt-2">
         <table class="table">
-  <thead>
-    <tr>
-      <th scope="col">id</th>
-      <th scope="col">product</th>
-      <th scope="col">price</th>
-      <th scope="col">qty</th>
-      <th scope="col">action</th>
-    </tr>
-  </thead>
-  <tbody>
-    <?php 
-         while($row = mysqli_fetch_assoc($query)){
-
-            echo "<tr><td>".$row["id"]."</td>";
-
-            echo "<td>".$row["product_name"]."</td>";
-            echo "<td>".$row["price"]."</td>";
-            echo "<td>".$row["qty"]."</td>";
-            echo "<td><button class='btn btn-danger'>delete</button></td></tr>";
-            }
-    ?>
-    <tr>
-      
-    </tr>
-   
-  </tbody>
-</table>
+            <thead>
+                <tr>
+                    <th scope="col">Id</th>
+                    <th scope="col">Product</th>
+                    <th scope="col">Price</th>
+                    <th scope="col">Qty</th>
+                    <th scope="col">Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php 
+                while($row = mysqli_fetch_assoc($query)){
+                    
+                    echo "<tr><td>".$row["id"]."</td>";
+                        
+                        echo "<td>".$row["product_name"]."</td>";
+                        echo "<td>".$row["price"]."</td>";
+                        echo "<td>".$row["qty"]."</td>";
+                        echo "<td>
+                            <button class='btn btn-danger'>Delete</button>
+                            <button class='btn btn-warning'>Edit</button>
+                        </td></tr>";
+                    }
+                    ?>
+                </tbody>
+            </table>
         </div>
+        
+        
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
+        <script src="scripts/index.js"></script>
+    </body>
+    </html>
     
-    
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
-    <script src="scripts/index.js"></script>
-</body>
-</html>
